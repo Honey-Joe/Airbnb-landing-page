4d9cb3b5f0bae86aab8703dded388eeab251597d
Honey-Joe <honeyjoe943@gmail.com>
2024-06-03 14:03:37 +0530

    updatr

---
+++ index.js
@@ -0,0 +1,44 @@
+
+
+var range = $('.input-range'),
+  value = $('.range-value');
+  var cont=$(".rangecounter")
+value.html(range.attr('value'));
+range.on('input', function() {
+  value.html(this.value);
+  cont.html(this.value*3666)
+});
+
+let count = document.querySelectorAll(".rangecounter")
+let arr = Array.from(count)
+
+
+
+arr.map(function(item){
+  let startnumber = 25600
+
+  function counterup(){
+  startnumber++
+  item.innerHTML= startnumber
+   
+  if(startnumber == item.dataset.number){
+      clearInterval(stop)
+  }
+}
+
+let stop =setInterval(function(){
+  counterup()
+},30)
+
+})
+
+
+$(document).scroll(function () {
+  var x = $(this).scrollTop();
+  var y = $(".banner-content-home").position();
+  if (x > y.top) {
+      $(".navigation-airhome").addClass("navigation-home");
+  } else {
+      $(".navigation-airhome").removeClass("navigation-home");
+  }
+});
--- script.js
+++ script.js
@@ -40,3 +40,5 @@ $('.responsive').slick({
       }
     ]
   });
+
+  
